f0a06c139c99b59c823163082d12a7b60df56b7a
pablo Trujillo <pablitott@gmail.com>
2021-12-31 15:23:30 -0500

    Production candidate 01

---
+++ AnsibleHost/scripts/createVBoxNetworks.sh
@@ -0,0 +1,47 @@
+#1/bin/bash
+#
+# Create the required VirtualBox network interfaces
+#
+
+#
+# First delete all existing interfaces. This will only work if no machines
+# using these interfaces are running
+#
+iflist=$(vboxmanage --nologo list hostonlyifs | egrep "^Name" | sed "s/Name://g")
+for if in $iflist; do
+  vboxmanage hostonlyif remove $if
+done
+
+#
+# Now create three new interfaces. They will be called vboxnet0, vboxnet1 and vboxnet2
+#
+vboxmanage hostonlyif create
+vboxmanage hostonlyif create
+vboxmanage hostonlyif create
+
+#
+# The first network (vboxnet0) will be our management interface. So we need to
+# set the IP address to 192.168.1.0/24 and make sure that there is no DCHP server
+#
+vboxmanage hostonlyif ipconfig vboxnet0 --netmask 255.255.255.0 --ip 192.168.1.1
+vboxmanage dhcpserver remove --netname HostInterfaceNetworking-vboxnet0
+
+vboxmanage hostonlyif ipconfig  "VirtualBox Host-Only Ethernet Adapter #2" --netmask 255.255.255.0 --ip 192.168.100.11
+vboxmanage hostonlyif ipconfig  "VirtualBox Host-Only Ethernet Adapter #3" --netmask 255.255.255.0 --ip 172.16.0.2
+
+"172.16.0.2",  auto_config: false, name: "VirtualBox Host-Only Ethernet Adapter #7"
+#
+# The second network will be our provider network. We use the CIDR 172.16.0.0/24
+# and disable DHCP as well
+#
+vboxmanage hostonlyif ipconfig vboxnet1 --netmask 255.255.255.0 --ip 172.16.0.1
+vboxmanage dhcpserver remove --netname HostInterfaceNetworking-vboxnet1
+
+
+#
+# Create some output to be able to check that everything worked
+#
+vboxmanage list hostonlyifs
+ip addr show dev vboxnet0
+ip addr show dev vboxnet1
+ip addr show dev vboxnet2
